5a46d8ae555b05326d25c90691ca4d75509d6dc9
Lauren Auchter <lsauchter@gmail.com>
2019-07-31 19:39:24 -0400

    Minor updates for production

--- server.js
+++ server.js
@@ -5,17 +5,15 @@ const cors = require('cors')
 const helmet= require('helmet')
 const POKEDEX = require('./pokedex.json')
 
-console.log(process.env.API_TOKEN)
-
 const app = express()
 
-app.use(morgan('dev'));
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common';
+app.use(morgan(morganSetting));
 app.use(helmet());
 app.use(cors());
 app.use(function validateBearerToken(req, res, next) {
     const authToken = req.get('Authorization');
     const apiToken = process.env.API_TOKEN
-    console.log('validate bearer token middleware');
 
     if (!authToken || authToken.split(' ')[1] !== apiToken) {
         return res.status(401).json({error: 'Unauthorized request'})
@@ -48,7 +46,17 @@ function handleGetPokemon(req, res) {
 }
 app.get('/pokemon', handleGetPokemon);
 
-const PORT = 8000
+app.use((error, req, res, next) => {
+    let response
+    if (process.env.NODE_ENV === 'production') {
+      response = { error: { message: 'server error' }}
+    } else {
+      response = { error }
+    }
+    res.status(500).json(response)
+});
+
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
